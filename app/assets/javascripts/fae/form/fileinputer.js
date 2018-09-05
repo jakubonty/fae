09d14ba9e196a37cd17cbfd140ac2281d4403011
Jakub Motycka <jakubonty@centrum.cz>
2018-09-05 10:56:59 +0200

    More translations

--- app/assets/javascripts/fae/form/fileinputer.js
+++ app/assets/javascripts/fae/form/fileinputer.js
@@ -160,8 +160,8 @@
     var defaults = {
       wrapper_class: 'asset-actions',
       button_class: 'button',
-      choose_text: 'Choose File',
-      file_text: 'No File Chosen',
+      choose_text: 'Vyberte soubor',
+      file_text: 'Soubor nebyl vybrán',
       active_class: 'active',
       delete_class: 'asset-delete'
     };
